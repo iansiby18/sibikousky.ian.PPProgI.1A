04bde8de8f012fb7d1aa1dcdc789cffd705c4844
iansiby18 <sibikousky14@gmail.com>
2022-10-24 09:46:14 -0300

    Parcial Finalizado

--- ParcialUno/src/ParcialUno.c
+++ ParcialUno/src/ParcialUno.c
@@ -10,8 +10,49 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+
+
+float aplicarDescuento(float precio, int descuento);
+int contarCaracteres(char cadena[], char caracter);
 
 int main(void) {
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
-	return EXIT_SUCCESS;
+
+	float precioDescuento;
+	char cadena[] = "aconcagua";
+	int contCaracter=0;
+
+
+	precioDescuento = aplicarDescuento(100,5);
+	printf("El precio con descuento es: %.2f", precioDescuento);
+
+	contCaracter= contarCaracteres(cadena, 'a');
+	printf("\nEl caracter se repite: %d", contCaracter);
 }
+
+
+float aplicarDescuento(float precio, int descuento){
+
+	float resultado;
+
+
+	resultado = precio - precio * descuento/100;
+
+	return resultado;
+}
+
+ int contarCaracteres(char cadena[], char caracter){
+
+	 int contCaracter=0;
+
+	 for(int i = 0; i < strlen(cadena); i++){
+		  if(cadena[i] == caracter){
+			  contCaracter= contCaracter +1;
+		  }
+	 }
+	 return contCaracter;
+}
+
+
+
